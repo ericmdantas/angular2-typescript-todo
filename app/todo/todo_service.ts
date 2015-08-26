53973f4b46f3034a25833ee93a8391d7d4089958
Eric Mendes Dantas <ericdantas0@gmail.com>
2015-08-26 07:24:12 -0300

    using eventemitter instead of rx implementation

--- app/todo/todo_service.ts
+++ app/todo/todo_service.ts
@@ -1,20 +1,20 @@
 /// <reference path="../../typings/tsd.d.ts" />
 
+import {EventEmitter} from 'angular2/angular2';
 import {TodoModel} from 'app/todo/todo_model.js';
-import * as Rx from 'rx';
 
 export class TodoService {
-    add(todo: TodoModel):Rx.Observable {
-        return Rx.Observable.create(o => {
-            o.onNext(todo);
-            o.onCompleted();
-        });
+    add(todo: TodoModel):EventEmitter {
+        let _ee = new EventEmitter();
+        setTimeout(() => _ee.next(todo));
+
+        return _ee.toRx();
     }
 
     remove(id: number):Rx.Observable {
-        return Rx.Observable.create(o => {
-            o.onNext(id);
-            o.onCompleted();
-        });
+        let _ee = new EventEmitter();
+        setTimeout(() => _ee.next(id));
+
+        return _ee.toRx();
     }
 }
