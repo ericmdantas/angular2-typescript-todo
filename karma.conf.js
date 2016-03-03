863ce4b3b6e3dc4f0b0ee104f83134885c9bf1b8
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-03-03 12:03:28 -0300

    attemp to fix travis build, #5

--- karma.conf.js
+++ karma.conf.js
@@ -35,6 +35,13 @@ module.exports = function(config) {
       "/app/": "/base/app/"
     },
 
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
+
     reporters: ['progress'],
     port: 9876,
     colors: true,
@@ -43,4 +50,8 @@ module.exports = function(config) {
     browsers: ['Chrome'],
     singleRun: true
   })
+
+  if (process.env.TRAVIS) {
+    config.browsers = ['Chrome_travis_ci'];
+  }
 }
