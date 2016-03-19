e503a5ccaf44b0cd675b8956b153d044a7c80ab4
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-03-18 22:30:04 -0300

    bumps typings version

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 /// <reference path="node_modules/angular2/typings/browser.d.ts" />
+/// <reference path="node_modules/zone.js/dist/zone.js.d.ts" />
 
 import {bootstrap} from 'angular2/platform/browser';
 import {TodoCmp} from './app/app';
