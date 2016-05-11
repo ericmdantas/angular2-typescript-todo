6e09a0bd6064306f520eaea702c45f029b44584d
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-05-11 10:01:07 -0300

    remove unecessary line to browser.d.ts in index.ts

--- index.ts
+++ index.ts
@@ -1,8 +1,3 @@
-/// <reference path="node_modules/@angular/common/index.d.ts" />
-/// <reference path="node_modules/@angular/compiler/index.d.ts" />
-/// <reference path="node_modules/@angular/core/index.d.ts" />
-/// <reference path="node_modules/@angular/platform-browser-dynamic/index.d.ts" />
-
 import {bootstrap} from '@angular/platform-browser-dynamic';
 import {TodoCmp} from './app/app';
 
