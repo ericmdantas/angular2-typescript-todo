1eaae01a6f8ae6c2578b953eecb7f44cf385e41c
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-08-11 07:17:08 -0300

    .

---
+++ app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule }       from '@angular/core';
+import { FormsModule }       from '@angular/forms';
+import { BrowserModule  } from '@angular/platform-browser';
+import { App }   from './app';
+import { TodoCmp }   from './todo/components/todo_cmp';
+import { TodoService }   from './todo/services/todo_service';
+import { todoRouting } from './todo/routes/todo_routes';
+
+@NgModule({
+    imports: [
+      BrowserModule,
+      FormsModule,
+      todoRouting
+    ],
+    declarations: [
+      App,
+      TodoCmp,
+    ],
+    providers: [
+      TodoService,
+    ],
+    bootstrap: [
+      App,
+    ],
+})
+export class AppModule {}
