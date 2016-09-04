f5495a5bd6f7f2caa41bf70b30458716e5e3d26e
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-09-04 20:58:31 -0300

    changes systemjs config

--- config.js
+++ config.js
@@ -1,44 +1,27 @@
 System.config({
     defaultJSExtensions: true,
     paths: {
-      '@angular/*': 'node_modules/@angular/*',
-      "rxjs/*": "node_modules/rxjs/*",
-      "reflect-metadata": "node_modules/reflect-metadata"
+      'npm:': 'node_modules/'
     },
     map: {
-      "rxjs": "node_modules/rxjs"
-    },
-    packages: {
-      '@angular/common': {
-        main: 'index'
-      },
-      '@angular/compiler': {
-        main: 'index'
-      },
-      '@angular/core': {
-        main: 'index'
-      },
-      '@angular/platform-browser-dynamic': {
-        main: 'index'
-      },
-      '@angular/platform-browser': {
-        main: 'index'
-      },
-      '@angular/router': {
-        main: 'index'
-      },
-      '@angular/forms': {
-        main: 'index'
-      },
-      '@angular/http': {
-        main: 'index'
-      },
-      "rxjs": {
-        defaultExtension: 'js'
-      },
-      'dist': {
-        defaultExtension: 'js',
-        format: 'register'
-      }
+      "rxjs": "node_modules/rxjs",
+      '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
+      '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
+      '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
+      '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
+      '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+      '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
+      '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
+      '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+
+      // angular testing umd bundles
+      '@angular/core/testing': 'npm:@angular/core/bundles/core-testing.umd.js',
+      '@angular/common/testing': 'npm:@angular/common/bundles/common-testing.umd.js',
+      '@angular/compiler/testing': 'npm:@angular/compiler/bundles/compiler-testing.umd.js',
+      '@angular/platform-browser/testing': 'npm:@angular/platform-browser/bundles/platform-browser-testing.umd.js',
+      '@angular/platform-browser-dynamic/testing': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic-testing.umd.js',
+      '@angular/http/testing': 'npm:@angular/http/bundles/http-testing.umd.js',
+      '@angular/router/testing': 'npm:@angular/router/bundles/router-testing.umd.js',
+      '@angular/forms/testing': 'npm:@angular/forms/bundles/forms-testing.umd.js',    
     }
   });
